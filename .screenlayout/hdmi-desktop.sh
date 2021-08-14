74e176c088895b153b9bf3e06e2861a031d0c99e
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2021-08-14 21:09:45 +0300

    new layout for new TV

--- .screenlayout/hdmi-desktop.sh
+++ .screenlayout/hdmi-desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 1280x720 --pos 0x0 --rotate normal --output DP1 --off --output eDP-1 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080i --pos 0x0 --rotate normal
